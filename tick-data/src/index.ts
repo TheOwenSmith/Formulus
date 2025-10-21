d8a418b7dbaa1199af3892dce2f2be205514c0d5
OwenSmith <osmith@cleverbenefits.com>
2025-10-21 15:52:54 -0500

    Remove d1-d2 algorithm

--- tick-data/src/index.ts
+++ tick-data/src/index.ts
@@ -1,12 +1,11 @@
 import { backtestAlgorithmsConcurrently, type Strategy } from './algorithms/backtest-algorithm';
-import { d1d2Algorithm } from './algorithms/d1-d2';
 import { prevBarAlgorithm } from './algorithms/prev-bar';
 import { tickDataFilenames } from './tick-data-files';
 import { tryAsync } from './utils/errorHandling';
 
 const constantSlippage = [0, 0.005, 0.01, 0.015, 0.02, 0.025];
 const bpsSlippage = [0, 0.2, 0.5, 1, 2, 5, 10];
-const algorithms = [prevBarAlgorithm, d1d2Algorithm];
+const algorithms = [prevBarAlgorithm];
 
 for (const file of tickDataFilenames) {
   const strategies: Strategy[] = [];
