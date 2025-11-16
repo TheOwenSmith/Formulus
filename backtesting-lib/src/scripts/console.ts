e2f680c96d7edeb29d1ab006437dc87eb44ce161
OwenSmith <osmith@cleverbenefits.com>
2025-11-16 11:01:54 -0600

    Uncomment script

--- backtesting-lib/src/scripts/console.ts
+++ backtesting-lib/src/scripts/console.ts
@@ -1,32 +1,33 @@
+import { fetchAlphaVantageData, type Ticker } from '@/fetch/fetch';
 import { testData } from './test-data';
 
-// const tickers: Ticker[] = [
-//   'SPY',
-//   'SPUU',
-//   'SPXL',
-//   'SH',
-//   'SDS',
-//   'SPXU',
-//   'QQQ',
-//   'NVDA',
-//   'TSLA',
-//   'AMD',
-//   'META',
-//   'AAPL',
-//   'MSFT',
-//   'AMZN',
-//   'GOOG',
-//   'PLTR',
-//   'SNAP',
-//   'PFE',
-// ];
-// for (const ticker of tickers) {
-//   await fetchAlphaVantageData({
-//     ticker,
-//     years: 20,
-//     timestamp: '60min',
-//   });
-// }
+const tickers: Ticker[] = [
+  'SPY',
+  'SPUU',
+  'SPXL',
+  'SH',
+  'SDS',
+  'SPXU',
+  'QQQ',
+  'NVDA',
+  'TSLA',
+  'AMD',
+  'META',
+  'AAPL',
+  'MSFT',
+  'AMZN',
+  'GOOG',
+  'PLTR',
+  'SNAP',
+  'PFE',
+];
+for (const ticker of tickers) {
+  await fetchAlphaVantageData({
+    ticker,
+    years: 20,
+    timestamp: '60min',
+  });
+}
 
 await testData([
   './data/uncleaned/SPY_60min.csv',
