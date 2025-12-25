59287346849eaff1f08ad4f9e64711503c9e7300
TheOwenSmith <owensmith@uchicago.edu>
2025-12-25 15:02:17 -0600

    Move api to web-app/api

--- api/src/lib/validate-env-vars.ts
+++
@@ -1,8 +0,0 @@
-export function validateEnvVars(envVars: string[]) {
-  const missingEnvVars = envVars.filter((envVar) => process.env[envVar] == undefined);
-  if (missingEnvVars.length > 0) {
-    throw new Error(
-      `Environment variable(s) '${missingEnvVars.join(', ')}' ${missingEnvVars.length > 1 ? 'are' : 'is'} not set`,
-    );
-  }
-}
