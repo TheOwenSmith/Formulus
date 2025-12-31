4980e252511890da2f1953c7264f67d409aa22ad
TheOwenSmith <owensmith@uchicago.edu>
2025-12-31 10:22:20 -0600

    Remove nodeplotlib

--- web-app/api/src/lib/nodeplotlib.ts
+++ web-app/api/src/lib/nodeplotlib.ts
@@ -1,5 +1,3 @@
-import { plot } from 'nodeplotlib';
-
 export type SimplePlot = {
   name: string;
   x: number[];
@@ -13,29 +11,3 @@ export type Graph = {
   algorithmName: string;
   description: string[];
 };
-
-export async function plotAlgorithm({
-  tickerPlot,
-  algorithmPlot,
-  algorithmName,
-  description,
-}: Graph) {
-  plot([algorithmPlot, tickerPlot], {
-    title: algorithmName,
-    xaxis: { title: 'Time Points' },
-    yaxis: { title: 'Portfolio Value ($)' },
-    annotations: [
-      {
-        bgcolor: 'rgba(255,255,255,0.8)',
-        bordercolor: 'rgba(0,0,0,0.3)',
-        borderwidth: 1,
-        showarrow: false,
-        text: description.join('<br>'),
-        x: 0.02,
-        xref: 'paper',
-        y: 0.98,
-        yref: 'paper',
-      },
-    ],
-  });
-}
