1704372f9ea598b86d7fcd1fb5a66424aa02eb60
TheOwenSmith <owensmith@uchicago.edu>
2026-02-08 15:53:45 -0600

    Refactor authentication

--- web-app/api/src/middleware/authentication.ts
+++ web-app/api/src/middleware/authentication.ts
@@ -1,8 +1,7 @@
 import { prisma } from '@api/lib/prisma';
 import type { TRPCContext } from '@api/lib/trpc';
-import { tryAsync } from '@api/utils/error-handling';
+import { fromThrowableAsync, internal, type AppError } from '@api/utils/error-handling';
 import { parseCookies } from '@api/utils/parse-cookies';
-import { TRPCError } from '@trpc/server';
 
 export function createUserAuthenticationProcedure(t: TRPCContext) {
   return t.procedure.use(async ({ ctx, next }) => {
@@ -10,32 +9,32 @@ export function createUserAuthenticationProcedure(t: TRPCContext) {
     const cookies = parseCookies(ctx);
     const sessionToken = cookies['better-auth.session_token']?.split('.', 1)[0];
     if (sessionToken == undefined) {
-      throw new TRPCError({
+      throw {
         code: 'UNAUTHORIZED',
         message: 'You must be logged in to perform this action',
-      });
+      } satisfies AppError;
     }
 
     // Get session from database
-    const getSessionResponse = await tryAsync(() =>
-      prisma.session.findUnique({
-        where: { token: sessionToken },
-        include: { user: true },
-      }),
+    const getSessionResponse = await fromThrowableAsync(
+      () =>
+        prisma.session.findUnique({
+          where: { token: sessionToken },
+          include: { user: true },
+        }),
+      (e) =>
+        internal(e, 'An unexpected error occurred while retrieving the session from the database'),
     );
-    if (!getSessionResponse.ok) {
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'An unexpected error occurred while retrieving the session from the database',
-      });
+    if (getSessionResponse.isErr()) {
+      throw getSessionResponse.error;
     }
-    const session = getSessionResponse.data;
+    const session = getSessionResponse.value;
 
     if (session == null || session.expiresAt < new Date()) {
-      throw new TRPCError({
+      throw {
         code: 'UNAUTHORIZED',
         message: 'Session expired or invalid',
-      });
+      } satisfies AppError;
     }
 
     return next({
