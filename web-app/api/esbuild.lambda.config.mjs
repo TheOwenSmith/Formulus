6ea508d6af4f35e63bc877fd069cfd3e7ea77411
TheOwenSmith <owensmith@uchicago.edu>
2026-05-09 18:14:24 -0500

    Fix prisma bundling error

--- web-app/api/esbuild.lambda.config.mjs
+++ web-app/api/esbuild.lambda.config.mjs
@@ -1,9 +1,12 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { build } from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Contain native code and cannot be bundled
+const prismaExternals = ['@prisma/client', '@prisma/client/*', '@prisma/adapter-pg', 'pg'];
 
 await build({
   entryPoints: [path.join(__dirname, 'src/lambda.ts')],
@@ -14,7 +17,7 @@ await build({
   outfile: path.join(__dirname, 'dist/lambda.js'),
   minify: true,
   sourcemap: true,
-  external: ['node:*'],
+  external: ['node:*', ...prismaExternals],
   plugins: [nodeExternalsPlugin()],
   alias: {
     '@api': path.join(__dirname, 'src'),
