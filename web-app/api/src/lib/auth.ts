d410722c4bf205561418fc0cd3c27a07138ba84c
TheOwenSmith <owensmith@uchicago.edu>
2026-06-03 17:25:34 -0500

    Remove pfp from session to prevent chunking

--- web-app/api/src/lib/auth.ts
+++ web-app/api/src/lib/auth.ts
@@ -1,5 +1,6 @@
-import { betterAuth } from 'better-auth';
+import { betterAuth, type User } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { customSession } from 'better-auth/plugins';
 import { config } from './config';
 import { prisma } from './prisma';
 
@@ -10,6 +11,13 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    customSession(async ({ user, session }) => {
+      const userWithoutImage: Omit<User, 'image'> = { ...user };
+      delete userWithoutImage['image'];
+      return { user: userWithoutImage, session };
+    }),
+  ],
   secret: config.getKey('BETTER_AUTH_SECRET'),
   socialProviders: {
     google: {
